aca91a2558547dece8fbb1dd41f0ed790e05b097
Hrishikesh Borkar <hrishikeshborkar94@gmail.com>
2026-04-06 23:42:54 +0530

    added instruction mod

---
+++ programs/sentinel-protocol/src/instructions/mod.rs
@@ -0,0 +1,15 @@
+pub mod init_dao;
+pub mod register_agent;
+pub mod arrest_agent;
+pub mod post_bail;
+pub mod cast_vote;
+pub mod release_agent;
+pub mod report_violation;
+
+pub use init_dao::*;
+pub use register_agent::*;
+pub use arrest_agent::*;
+pub use post_bail::*;
+pub use cast_vote::*;
+pub use release_agent::*;
+pub use report_violation::*;
